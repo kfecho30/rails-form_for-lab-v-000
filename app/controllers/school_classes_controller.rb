98d7373517a977d9efac103ae7f220977cbbd17c
Kelsey Fecho <kfecho30@gmail.com>
2018-04-29 00:35:29 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -21,5 +21,8 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
+    @class = SchoolClass.find(params[:id])
+    @class.update(params.require(:school_class).permit(:title, :room_number))
+    redirect_to school_class_path(@class)
   end
 end

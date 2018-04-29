3e1b624bb398cc755eec6168b3fdabdd52b08b43
Kelsey Fecho <kfecho30@gmail.com>
2018-04-29 00:10:23 +0000

    automatically backed up by learn

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,20 @@
+class StudentsController < ApplicationController
+  def index
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
